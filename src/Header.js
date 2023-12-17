080365220e255658ed50cc35551b8b6001a383cb
Shantanu Ambadkar <89132314+shantanuambadkar@users.noreply.github.com>
2023-12-17 21:05:20 +0530

    Header changes

--- src/Header.js
+++ src/Header.js
@@ -3,18 +3,22 @@
 import React from 'react';
 import './Header.css'; // Import the CSS file for styling
 import { AppBar, Toolbar, Typography, Container } from '@mui/material';
-import logo from './1.png';
+import logo from './Logo.png';
 
 const Header = () => {
   return (
     <div className="sticky-header">
       <div className="mobile-contact-us-header">
-        <Typography component="div" sx={{ flexGrow: 1 }}>
+        <Typography component="div" sx={{ flexGrow: 1 }} className="small-font">
           +91-8828823099 / +91-9819316013
         </Typography>
-        <Typography component="div" sx={{ flexGrow: 1 }} className="end-text">
+        <Typography
+          component="div"
+          sx={{ flexGrow: 1 }}
+          className="end-text small-font"
+        >
           <a
-            href="https://wa.me/8828823099"
+            href="https://wa.me/+918828823099"
             className="aTagColor"
             target="_blank"
             rel="noopener noreferrer"
@@ -29,7 +33,7 @@ const Header = () => {
             <img
               src={logo}
               alt="Logo"
-              style={{ width: '60px', height: 'auto' }}
+              style={{ width: '10%', height: 'auto' }}
             />
             <Typography variant="h3" component="div" sx={{ flexGrow: 1 }}>
               Cementers
