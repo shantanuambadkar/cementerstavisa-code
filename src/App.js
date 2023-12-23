3cc9d8bc411e0ca61d527f941d813bb68a49e266
Shantanu Ambadkar <89132314+shantanuambadkar@users.noreply.github.com>
2023-12-23 14:15:40 +0530

    Gtag code added

--- src/App.js
+++ src/App.js
@@ -189,7 +189,23 @@ function App() {
     });
   };
 
+  function gtag_report_conversion(url) {
+    var gtag = window.gtag || function () {};
+    var callback = function () {
+      if (typeof url != 'undefined') {
+        window.location = url;
+      }
+    };
+    gtag('event', 'conversion', {
+      send_to: 'AW-11442235311/dC7SCJGel4AZEK_PitAq',
+      event_callback: callback,
+    });
+    return false;
+  }
+
   const postToGoogleSheet = async () => {
+    gtag_report_conversion('https://cementers.co.in');
+
     try {
       await fetch(
         url +
